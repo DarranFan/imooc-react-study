3a42b828e8ce3eec7af262a879f26ef6e3e06899
unknown <da.fan@xinboxinmo.com>
2018-09-08 17:32:39 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './App.less';
+import { Button } from 'antd';
+class App extends Component {
+  render() {
+    return (
+      <div className="content">
+        <div>11111111111</div>
+        <Button type="dashed">按钮</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+export default class NavLeft extends React.Component{
+    render(){
+        // var style = {
+        //     backgroundColor:'red'
+        // }
+        return(
+            <div>
+                this is navlsft
+            </div>
+        )
+    }
+}
